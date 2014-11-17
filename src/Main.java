304b2c93b98a904965be1a169fb9057aa387918d
griga <griga@ukr.net>
2014-11-17 23:20:41 +0200

    добавил сервер, изменил line на char, разбираюсь с передачей потоков

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-package test;
+
 
 public class Main {
 
 	public static void main(String[] args) {
+		Server server =  new Server();
+		server.run();
 		// TODO Auto-generated method stub
 		System.out.println("Hello :)");
 	}
-
 }
